11a7158473f03167e19284725293e8e3257e194f
myles-mikido <michaelmacharia6699@gmail.com>
2025-01-31 12:40:52 +0300

    python1

---
+++ basics1.py
@@ -0,0 +1,32 @@
+#display text
+print("This is a python class")
+print("Hello World")
+print("700")
+#variables are named containers for starting values
+fname="Jane"
+lname="Joy"
+age=18
+course="cybersecurity"
+Gender="Female"
+location="Kiambu, Gachie"
+status="Married"
+
+#print the values
+print(fname)
+print(lname)
+print(age)
+print(course)
+
+#displaying a string and a variable
+print("Hello, my name is",lname)
+print("i am",age,"years old")
+print("i am a",Gender)
+print("I come from a place called",location)
+print("i am",status,"to Michael")
+print("My name is",lname,"i am",age,"years old","i am a",Gender,"and i'm currently",status,"to Michael")
+
+x=("Joy")
+y=("is")
+z=("beautiful")
+
+print(x,y,z)
